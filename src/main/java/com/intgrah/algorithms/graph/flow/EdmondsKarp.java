c9982b5c54fcc2b102ae7b63ec45f8200b89ac2f
IntGrah <148660026+IntGrah@users.noreply.github.com>
2024-04-13 00:08:49 +0100

    EKA

---
+++ src/main/java/com/intgrah/algorithms/graph/flow/EdmondsKarp.java
@@ -0,0 +1,19 @@
+package com.intgrah.algorithms.graph.flow;
+
+import com.intgrah.algorithms.graph.Graph;
+import com.intgrah.algorithms.util.OrderedSemigroup;
+
+public class EdmondsKarp<V, W> {
+
+    private final OrderedSemigroup<W> osg;
+
+    public EdmondsKarp(OrderedSemigroup<W> osg) {
+        this.osg = osg;
+    }
+
+
+    public Graph<V, W> maxFlow() {
+        return null;
+    }
+
+}
